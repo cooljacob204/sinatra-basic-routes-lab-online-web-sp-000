d5135511bd4f53fed108022089c9c04a7c60e4ae
Jacob Feldman <jacobgfeld@gmail.com>
2019-03-18 16:48:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,12 @@ class App < Sinatra::Base
   get '/name' do
     "My name is Jacob"
   end
+  
+  get '/hometown' do
+    'My hometown is Newyork'
+  end
+  
+  get '/favorite-song' do
+    'My favorite song is i don\'t have one lol'
+  end
 end
